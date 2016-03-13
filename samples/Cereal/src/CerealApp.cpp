06a2a7c518e2a05bbefcf3e2e9cb683fa289712d
simongeilfus <simongeilfus@gmail.com>
2016-03-13 15:15:35 +0100

    Added basic sample

--- samples/Cereal/src/CerealApp.cpp
+++ samples/Cereal/src/CerealApp.cpp
@@ -3,35 +3,36 @@
 #include "cinder/gl/gl.h"
 
 #include "CinderCereal.h"
-#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/vector.hpp>
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+// See http://uscilab.github.io/cereal/index.html for more examples
+
 class CerealApp : public App {
   public:
-	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-	void update() override;
-	void draw() override;
+	void setup() override
+	{
+		// create a JSON archive that output to the console
+		cereal::JSONOutputArchive archive( console() );
+		
+		// some data to serialize
+		bool arr[] = {true, false};
+		vector<int> vec = {1, 2, 3, 4, 5};
+		vector<vec3> vecOfVec3 = { vec3(1), vec3(2), vec3(3,4,5) };
+		CameraPersp camera( getWindowWidth(), getWindowHeight(), 50.0f, 0.1f, 100.0f );
+		fs::path p = getAppPath();
+		auto args = getCommandLineArgs();
+
+		// serialize
+		archive( vec, arr, CEREAL_NVP(vecOfVec3), CEREAL_NVP(camera), getWindowBounds(), getWindowPos(), p, args );
+		
+		// quit the app
+		quit();
+	}
 };
-
-void CerealApp::setup()
-{
-}
-
-void CerealApp::mouseDown( MouseEvent event )
-{
-}
-
-void CerealApp::update()
-{
-}
-
-void CerealApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) ); 
-}
 
 CINDER_APP( CerealApp, RendererGl )
