097739b1873ffc8bc78d8cecd799d926104383f4
origamidance <origamidance@gmail.com>
2019-10-21 20:17:58 +0200

    update for new cinder library

--- proj/cmake/Cinder-CerealConfig.cmake
+++ proj/cmake/Cinder-CerealConfig.cmake
@@ -1,21 +1,25 @@
 if (NOT TARGET Cinder-Cereal)
     get_filename_component(CINDER-CEREAL_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
     get_filename_component(CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE)
-    list(APPEND CEREAL_SOURCES
-            ${CINDER-CEREAL_PATH}/include/CinderCereal.h
-            )
-    add_library(Cinder-Cereal
-            ${CEREAL_SOURCES})
-    target_include_directories(Cinder-Cereal PUBLIC
-            ${CINDER-CEREAL_PATH}/lib/cereal/include
-            ${CINDER-CEREAL_PATH}/include)
+    list(APPEND Cinder-Cereal_INCLUDES
+      ${CINDER-CEREAL_PATH}/include/
+      ${CINDER-CEREAL_PATH}/lib/cereal/include
+      )
+    # list(APPEND CEREAL_SOURCES
+    #         ${CINDER-CEREAL_PATH}/include/CinderCereal.h
+    #         )
+    # add_library(Cinder-Cereal
+    #         ${CEREAL_SOURCES})
+    # target_include_directories(Cinder-Cereal PUBLIC
+    #         ${CINDER-CEREAL_PATH}/lib/cereal/include
+    #         ${CINDER-CEREAL_PATH}/include)
 
-    if (NOT TARGET cinder)
-        include("${CINDER_PATH}/proj/cmake/configure.cmake")
-        find_package(cinder REQUIRED PATHS
-                "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
-                "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
-    endif ()
+    # if (NOT TARGET cinder)
+    #     include("${CINDER_PATH}/proj/cmake/configure.cmake")
+    #     find_package(cinder REQUIRED PATHS
+    #             "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
+    #             "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
+    # endif ()
 
-    target_link_libraries(Cinder-Cereal PRIVATE cinder)
+    # target_link_libraries(Cinder-Cereal PRIVATE cinder)
 endif ()
